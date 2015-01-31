55d73ad0f2955ab58bb89a849c16b214e41bcaae
Dylan Watson <watson93@gmail.com>
2015-01-31 17:16:21 -0500

    Added models for Machine and threat.  Created DB based on models.

--- vent/topology/models.py
+++ vent/topology/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Machine(models.Model):
+    name = models.CharField(max_length=16, unique=True) #SSH-Server
+    ip = models.CharField(max_length=15, unique=True) #255.255.255.255
+    number_of_threats = models.IntegerField(default=0)
+    threat_level = models.IntegerField(default=0, max_value=4) #0-4
+
+    def __unicode__(self):
+        return self.ip
+
+class Threat(models.Model):
+    name = models.CharField(max_lenth=64)
+    attacker = models.CharField(max_length=15, unique=True) #255.255.255.255
+    reciever = models.ForeignKey(Machine)
+    date = models.DateTimeField('date created', default=datetime.now)
+
+    def __unicode__(self):
+        return self.name
