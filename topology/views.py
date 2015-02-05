ef513b872faa9e67c62e4b003ee924cccc6baa60
Dylan Watson <watson93@gmail.com>
2015-02-04 23:30:25 -0500

    Removed unneeded view context dictionary item.

--- topology/views.py
+++ topology/views.py
@@ -6,6 +6,5 @@ def index(request):
     context_dict = {}
     machine_list = Machine.objects.all()
     context_dict['machines'] = machine_list
-    context_dict['number_of_machines'] = range(0, len(machine_list))
 
     return render(request, 'topology/index.html', context_dict)
