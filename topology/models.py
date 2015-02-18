1e127d959d3754a30075f0e8695335b54dacc35d
Dylan Watson <watson93@gmail.com>
2015-02-18 10:23:41 -0500

    Added new images to represent different threat levels.  Modified model to go from 0-3 instead of 0-4.  Added additional machines to database with more fun threat levels.

--- topology/models.py
+++ topology/models.py
@@ -5,7 +5,7 @@ class Machine(models.Model):
     name = models.CharField(max_length=16, unique=True) #SSH-Server
     ip = models.CharField(max_length=15, unique=True) #255.255.255.255
     number_of_threats = models.IntegerField(default=0)
-    threat_level = models.IntegerField(default=0) #0-4
+    threat_level = models.IntegerField(default=0) #0-3
 
     def __unicode__(self):
         return self.name
