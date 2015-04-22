347fd8d982a06d218558cb98aa0c6ee2aa43e86d
Dylan Watson <watson93@gmail.com>
2015-04-22 19:56:45 -0400

    Modified HTML pages to suit new url pattern, now following /topology/attacker/<ip>.html.  Upon reflection, since we are listing all the attacks from that attacker, it made more sense to have it not be associated with the particular reciever of the attacker in the URL pattern.  I also made a few notes of where I want the button to blackhole IP addresses and the button to add a name to the machine

--- topology/urls.py
+++ topology/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from topology import views
 
 urlpatterns = patterns('', url(r'^$', views.index, name='index'),
-url(r'^machine/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/attacker/(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/$',views.attacker, name='attacker'),
+url(r'^attacker/(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/$',views.attacker, name='attacker'),
 url(r'^machine/(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/$',views.machine, name='machine'),)
