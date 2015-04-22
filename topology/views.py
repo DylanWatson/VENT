21b314bb6cf7d0fd56ef221d52f6fa786bf9eca1
Dylan Watson <watson93@gmail.com>
2015-04-22 19:48:41 -0400

    Created attacker.html to display information about the attacker.  Modified urls.py to add correct pattern for attacker.html and made machine.html more strict.  Modified views.py to include attacker and added context_dict for it to access all of the machines that the attacker attacked.

--- topology/views.py
+++ topology/views.py
@@ -16,9 +16,38 @@ def machine(request, ip):
         context_dict['machine_ip'] = machine
 
         threats = Threat.objects.filter(reciever=ip)
-        context_dict['threats'] = threats 
+        context_dict['threats'] = threats
+        attackers = []
+        for threat in threats:
+            attackers.append(str(threat.attacker))
+        unique_attackers = list(set(attackers))
+        attacker_and_attacks = []
+        for unique_attacker in unique_attackers:
+            count = 0
+            for threat in threats:
+                if unique_attacker == threat.attacker:
+                    count = count + 1
+            attacker_and_attacks.append((unique_attacker, count))
+
+        print attacker_and_attacks
+        #Sort by number of threats
+        #Save top 5 in context_dict so it can be accessed by machine.html
+        #Maybe make cool visualizations? :)
+
+
+
 
     except:
         pass
 
+
     return render(request, 'topology/machine.html', context_dict)
+
+
+def attacker(request, ip):
+    context_dict = {}
+
+    threats = Threat.objects.filter(attacker=ip)
+    context_dict['attacker'] = ip
+    context_dict['threats'] = threats
+    return render(request, "topology/attacker.html", context_dict)
